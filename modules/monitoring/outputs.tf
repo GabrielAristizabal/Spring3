84db58b471df212d9ddbcaa96b32865a640ecf66
Gabriel Aristizabal <g.aristizabal@uniandes.edu.co>
2025-11-05 13:23:58 -0500

    errores

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -8,5 +8,3 @@ output "dashboard_url" {
   value = "https://${data.aws_region.current.name}.console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.name}#dashboards:name=${aws_cloudwatch_dashboard.main.dashboard_name}"
 }
 
-data "aws_region" "current" {}
-
