e69fc41c1b721aebf7dd94dd76863037e2dafa12
Gabriel Aristizabal <g.aristizabal@uniandes.edu.co>
2025-11-05 20:52:56 -0500

    url

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from orders.views import home  # importa la vista directamente
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("orders.urls")),   # ← envía todo a la app Order
+    path("", home, name="home"),   # home en la raíz
+    # path("order/create/", create_order, name="order_create"),
 ]
