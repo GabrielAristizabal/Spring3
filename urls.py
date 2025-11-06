85cbdbdbe0b0500a28bb0c5b6b4e021d6015afa2
Gabriel Aristizabal <g.aristizabal@uniandes.edu.co>
2025-11-05 20:39:45 -0500

    commit

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import home  # ajusta si tu vista se llama distinto
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("orders.urls")),   # ← envía todo a la app Orders
+    path("", include("orders.urls")),   # ← envía todo a la app Order
 ]
