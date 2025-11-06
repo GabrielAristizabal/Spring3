d45230c694674e8bd2c96f4c229d6e6696f483f0
Gabriel Aristizabal <g.aristizabal@uniandes.edu.co>
2025-11-05 20:36:54 -0500

    order

--- Orders/Urls.py
+++ Orders/Urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import home
 
-app_name = "orders"
+app_name = "orders" #minuscula
 urlpatterns = [
     path("", home, name="home"),  # "/" muestra el Home
 ]
