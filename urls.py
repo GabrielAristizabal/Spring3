d85f9470eeee2570be8cf3e8856e95da3f39f979
SebasRodriT <js.rodriguezt1@uniandes.edu.co>
2025-11-05 19:25:41 -0500

    agg

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Orders.views import home  # ajusta si tu vista se llama distinto
+from .views import home  # ajusta si tu vista se llama distinto
 
 urlpatterns = [
     path("admin/", admin.site.urls),
