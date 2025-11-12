71751802a96bdf8817c23e9636069a421d84a341
SebasRodriT <js.rodriguezt1@uniandes.edu.co>
2025-11-11 20:42:27 -0500

    C

--- pedidos-app/project/urls.py
+++ pedidos-app/project/urls.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from orders.views import (
-    home, order_list, order_detail, order_create, order_update, order_delete,
-    register_user, verify
+    create_order, register_user, set_signer,
+    order_list, order_detail, order_update, order_delete, verify,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(pattern_name="create_order", permanent=False), name="home"),
+    path("", RedirectView.as_view(pattern_name="order_create", permanent=False), name="home"),
+    path("users/register/", register_user, name="register_user"),
+    path("firmante/usar/", set_signer, name="set_signer"),
     path("orders/", order_list, name="order_list"),
-    path("orders/create/", order_create, name="order_create"),
+    path("orders/create/", create_order, name="order_create"),
     path("orders/<str:order_id>/", order_detail, name="order_detail"),
     path("orders/<str:order_id>/edit/", order_update, name="order_update"),
     path("orders/<str:order_id>/delete/", order_delete, name="order_delete"),
-    path("users/register/", register_user, name="register_user"),
     path("verify/", verify, name="verify"),
 ]
